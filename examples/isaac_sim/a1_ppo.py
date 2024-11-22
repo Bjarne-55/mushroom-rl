8720ecb23cf6849f6f47513cd20ce922127b37c8
Bjarne-55 <73470930+Bjarne-55@users.noreply.github.com>
2024-11-22 09:44:44 +0100

    Improve a1

--- examples/isaac_sim/a1_ppo.py
+++ examples/isaac_sim/a1_ppo.py
@@ -6,6 +6,9 @@ import torch.optim as optim
 import numpy as np
 from tqdm import trange
 
+import random
+import time
+
 from mushroom_rl.core import VectorCore, Logger
 from mushroom_rl.algorithms.actor_critic import TRPO, PPO
 
@@ -47,13 +50,17 @@ def experiment(alg, n_epochs, n_steps, n_steps_per_fit, n_episodes_test,
     logger.strong_line()
     logger.info('Experiment Algorithm: ' + alg.__name__)
 
-    mdp = IsaacA1Description(10)
+    mdp = IsaacA1Description(64, 200, True)
+    mdp.seed(0)#Episode 16; 57%
+    torch.manual_seed(0)
+    random.seed(0)
+    np.random.seed(0)
     
     critic_params = dict(network=Network,
                          optimizer={'class': optim.Adam,
                                     'params': {'lr': 3e-4}},
                          loss=F.mse_loss,
-                         n_features=32,
+                         n_features=256,
                          batch_size=64,
                          use_cuda=True,
                          input_shape=mdp.info.observation_space.shape,
@@ -67,11 +74,12 @@ def experiment(alg, n_epochs, n_steps, n_steps_per_fit, n_episodes_test,
     alg_params['critic_params'] = critic_params
 
     agent = alg(mdp.info, policy, **alg_params)
+    agent.load("stored_agents/a1_ppo/1732168213.793614.zip")
     #agent.set_logger(logger)
 
     core = VectorCore(agent, mdp)
 
-    dataset = core.evaluate(n_episodes=n_episodes_test, render=True, record=False)
+    dataset = core.evaluate(n_episodes=n_episodes_test, render=True, record=True)
 
     J = torch.mean(dataset.discounted_return).item()
     R = torch.mean(dataset.undiscounted_return).item()
@@ -80,8 +88,8 @@ def experiment(alg, n_epochs, n_steps, n_steps_per_fit, n_episodes_test,
     logger.epoch_info(0, J=J, R=R, entropy=E)
 
     for it in trange(n_epochs, leave=False):
-        core.learn(n_steps=n_steps, n_steps_per_fit=n_steps_per_fit)
-        dataset = core.evaluate(n_episodes=n_episodes_test, render=True, record=False)
+        #core.learn(n_steps=n_steps, n_steps_per_fit=n_steps_per_fit)
+        dataset = core.evaluate(n_episodes=n_episodes_test, render=True, record=True)
 
         J = torch.mean(dataset.discounted_return).item()
         R = torch.mean(dataset.undiscounted_return).item()
@@ -89,9 +97,10 @@ def experiment(alg, n_epochs, n_steps, n_steps_per_fit, n_episodes_test,
 
         logger.epoch_info(it+1, J=J, R=R, entropy=E)
 
-    logger.info('Press a button to visualize')
-    input()
-    core.evaluate(n_episodes=5, render=True, record=False)
+    #logger.info('Press a button to visualize')
+    #input()
+    core.evaluate(n_episodes=n_episodes_test, render=True, record=True)
+    #agent.save(f"stored_agents/a1_ppo/{str(time.time())}.zip")
 
 
 if __name__ == '__main__':
@@ -106,9 +115,9 @@ if __name__ == '__main__':
     )
     policy_params = dict(
         std_0=1.,
-        n_features=32,
+        n_features=256,
         use_cuda=True
 
     )
-    experiment(alg=PPO, n_epochs=40, n_steps=30000, n_steps_per_fit=3000,
-                   n_episodes_test=512, alg_params=ppo_params, policy_params=policy_params)
+    experiment(alg=PPO, n_epochs=40, n_steps=200000, n_steps_per_fit=10000,
+                   n_episodes_test=64, alg_params=ppo_params, policy_params=policy_params)
