4b3d91500a681f1e840986edecb7eb07044d348d
Bjarne-55 <73470930+Bjarne-55@users.noreply.github.com>
2025-02-12 13:12:12 +0100

    WIP: HoneyBadger

--- examples/isaac_sim/honey_batcher_ppo_plotting.py
+++ examples/isaac_sim/honey_batcher_ppo_plotting.py
@@ -58,7 +58,7 @@ def experiment(alg, num_envs, n_epochs, n_steps, n_steps_per_fit, n_episodes_tes
     logger.strong_line()
     logger.info('Experiment Algorithm: ' + alg.__name__)
 
-    mdp = HoneyBatcher(num_envs, 1000, True)
+    mdp = HoneyBatcher(num_envs, 1000, True, False)
     mdp.seed(seed)
     
     critic_params = dict(network=Network,
@@ -79,7 +79,7 @@ def experiment(alg, num_envs, n_epochs, n_steps, n_steps_per_fit, n_episodes_tes
     alg_params['critic_params'] = critic_params
 
     agent = alg(mdp.info, policy, **alg_params)
-    #agent = agent.load("/home/bjarne/GitWorkspace/BachelorThesis/mushroom-rl/stored_agents/a1_ppo/1737244844.636734.zip")
+    #agent = agent.load("/home/bjarne/GitWorkspace/BachelorThesis/mushroom-rl/stored_agents/a1_ppo/1738729987.1072295.zip")
     #agent.set_logger(logger)
 
     core = VectorCore(agent, mdp)
@@ -90,8 +90,7 @@ def experiment(alg, num_envs, n_epochs, n_steps, n_steps_per_fit, n_episodes_tes
     Vs = []
     INFOs = []
 
-    """
-    dataset = core.evaluate(n_episodes=n_episodes_test, render=True, record=True)
+    dataset = core.evaluate(n_episodes=n_episodes_test, render=False, record=False)
 
     J = torch.mean(dataset.discounted_return).to("cpu").item()
     R = torch.mean(dataset.undiscounted_return.to("cpu")).item()
@@ -105,11 +104,13 @@ def experiment(alg, num_envs, n_epochs, n_steps, n_steps_per_fit, n_episodes_tes
     INFOs.append(INFO)
 
     logger.epoch_info(0, J=J, R=R, entropy=E, V=V)
-    """
 
     for it in trange(n_epochs, leave=False):
         core.learn(n_steps=n_steps, n_steps_per_fit=n_steps_per_fit)
-        dataset = core.evaluate(n_episodes=n_episodes_test, render=True, record=True)
+        if it % 5 == 0:
+            dataset = core.evaluate(n_episodes=n_episodes_test, render=True, record=True)
+        else:
+            dataset = core.evaluate(n_episodes=n_episodes_test, render=False, record=False)
         agent.save(f"stored_agents/a1_ppo/{str(time.time())}.zip", True)
 
         J = torch.mean(dataset.discounted_return).to("cpu").item()
@@ -158,8 +159,8 @@ if __name__ == '__main__':
     num_envs = 4096
 
     seed = np.random.randint(0, 10000)
-    Js, Rs, Es, Vs, INFOs = experiment(alg=PPO, num_envs=num_envs, n_epochs=40, n_steps=4096*24*50, n_steps_per_fit=4096*24,
-                   n_episodes_test=256, alg_params=ppo_params, policy_params=policy_params, seed=seed)
+    Js, Rs, Es, Vs, INFOs = experiment(alg=PPO, num_envs=num_envs, n_epochs=10, n_steps=4096*24*200, n_steps_per_fit=4096*24,
+                   n_episodes_test=128, alg_params=ppo_params, policy_params=policy_params, seed=seed)
     
     dir = "plots/a1_effort_ppo/" + str(time.time())
     os.makedirs(dir)
