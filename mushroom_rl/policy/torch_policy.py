c9c7547c6733408f40999f6f2651508f8946a7ec
Bjarne-55 <73470930+Bjarne-55@users.noreply.github.com>
2025-02-12 13:04:55 +0100

    Revert wrong change

--- mushroom_rl/policy/torch_policy.py
+++ mushroom_rl/policy/torch_policy.py
@@ -207,7 +207,7 @@ class GaussianTorchPolicy(TorchPolicy):
         return self.distribution_t(state).sample().detach()
 
     def log_prob_t(self, state, action):
-        return self.distribution_t(state).log_prob(action.to(dtype=torch.float32))[:, None]
+        return self.distribution_t(state).log_prob(action)[:, None]
 
     def entropy_t(self, state=None):
         return self._action_dim / 2 * torch.log(TorchUtils.to_float_tensor(2 * np.pi * np.e))\
