2f99b55a7dbd4fdcb64b1f56f32b22e4ac42df7d
Bjarne-55 <73470930+Bjarne-55@users.noreply.github.com>
2025-02-13 22:31:27 +0100

    Revert wrong change in nikita ppo

--- mushroom_rl/algorithms/actor_critic/deep_actor_critic/ppo_nikita.py
+++ mushroom_rl/algorithms/actor_critic/deep_actor_critic/ppo_nikita.py
@@ -36,7 +36,7 @@ class NikitaPPO(PPO):
 
         old_pol_dist = self.policy.distribution_t(state_old)
 
-        old_log_p = old_pol_dist.log_prob(action.to(dtype=torch.float32))[:, None].detach()
+        old_log_p = old_pol_dist.log_prob(action)[:, None].detach()
 
         self._V.fit(state, v_target, **self._critic_fit_params)
 
