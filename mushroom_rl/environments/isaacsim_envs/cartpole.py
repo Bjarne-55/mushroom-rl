636bc5398cca97639664ff62d773e54d01b9395d
Bjarne-55 <73470930+Bjarne-55@users.noreply.github.com>
2025-04-10 04:31:03 +0200

    Allow cartpole to be run with different backends

--- mushroom_rl/environments/isaacsim_envs/cartpole.py
+++ mushroom_rl/environments/isaacsim_envs/cartpole.py
@@ -1,13 +1,12 @@
 from mushroom_rl.environments import IsaacSim
 from mushroom_rl.utils.isaac_sim import ObservationType
+from mushroom_rl.core import ArrayBackend
+
 import numpy as np
 import torch
 
 class CartPole(IsaacSim):
-    def __init__(self, num_envs):
-        backend="torch"
-        device="cuda:0"
-
+    def __init__(self, num_envs, backend="torch", device="cuda:0"):
         usd_path = "/home/bjarne/GitWorkspace/BachelorThesis/mushroom-rl/isaac_assets/cartpole_slope.usd"
         action_spec = ["rail_cart_joint"]
         observation_spec = [
@@ -26,28 +25,30 @@ class CartPole(IsaacSim):
         super().__init__(usd_path, action_spec, observation_spec, backend, device, collision_between_envs, num_envs, 
                          env_spacing, 0.99, 200, additional_data_spec=additional_data_spec)
         
+        self.backend = ArrayBackend.get_array_backend(backend)
+        
     def reward(self, obs, action, next_obs, absorbing):
         pole_joint_pos = self.observation_helper.get_from_obs(next_obs, "poleJointPos").squeeze()
         cart_joint_pos = self.observation_helper.get_from_obs(next_obs, "cartJointPos").squeeze()
-        reward = 1.0 - torch.abs(cart_joint_pos)
-        reward = torch.where(absorbing, -torch.ones_like(pole_joint_pos), reward)
+        reward = 1.0 - self.backend.abs(cart_joint_pos)
+        reward = self.backend.where(absorbing, -self.backend.ones_like(pole_joint_pos), reward)
         return reward
 
     def is_absorbing(self, obs):
         pole_joint_pos = self.observation_helper.get_from_obs(obs, "poleJointPos").squeeze()
-        ones = torch.ones_like(pole_joint_pos, dtype=bool)
-        zeros = torch.zeros_like(pole_joint_pos, dtype=bool)
-        dropped = torch.where(torch.abs(pole_joint_pos) > np.pi / 2, ones, zeros)
+        ones = self.backend.ones_like(pole_joint_pos, dtype=bool)
+        zeros = self.backend.zeros_like(pole_joint_pos, dtype=bool)
+        dropped = self.backend.where(self.backend.abs(pole_joint_pos) > np.pi / 2, ones, zeros)
         return dropped
 
     def setup(self, env_indices, obs):
         num_environments = len(env_indices)
 
-        cart_joint_pos = 0.25 * (2.0 * torch.rand(num_environments, 1, device=self._device) - 1)
-        pole_joint_pos = 0.05 * np.pi * (2.0 * torch.rand(num_environments, 1, device=self._device) - 1)
+        cart_joint_pos = 0.25 * (2.0 * self.backend.rand(num_environments, 1, device=self._device) - 1)
+        pole_joint_pos = 0.05 * np.pi * (2.0 * self.backend.rand(num_environments, 1, device=self._device) - 1)
 
-        cart_joint_vel = 0.25 * (2.0 * torch.rand(num_environments, 1, device=self._device) - 1)
-        pole_joint_vel = 0.05 * np.pi * (2.0 * torch.rand(num_environments, 1, device=self._device) - 1)
+        cart_joint_vel = 0.25 * (2.0 * self.backend.rand(num_environments, 1, device=self._device) - 1)
+        pole_joint_vel = 0.05 * np.pi * (2.0 * self.backend.rand(num_environments, 1, device=self._device) - 1)
 
         self._write_data("cartJointPos", cart_joint_pos, env_indices)
         self._write_data("poleJointPos", pole_joint_pos, env_indices)
